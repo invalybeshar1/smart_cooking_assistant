0bfcb24f897b7f0bff338aa5e25d7547c9db6235
invalybeshar1 <ilybeshari22@epoka.edu.al>
2025-05-07 17:35:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ import cors from 'cors';
 import registerRoute from './routes/register.js'; 
 import questionnaireRoute from './routes/questionnaire.js';
 import loginRoute from './routes/login.js';
-
+import userRoute from './routes/user.js';
+import chatRoute from './routes/chat.js';
 dotenv.config();
 
 const app = express();
@@ -19,6 +20,8 @@ app.use(express.json());
 app.use('/api/register', registerRoute); 
 app.use('/api/questionnaire', questionnaireRoute);
 app.use('/api/login', loginRoute);
+app.use('/api/user', userRoute);
+app.use('/api/chat', chatRoute);
 
 // Test route
 app.get('/', (req, res) => {
